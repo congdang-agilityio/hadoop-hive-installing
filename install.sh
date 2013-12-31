bb0b5f53b7c83e5e47350223a8a2573b8a8fe5de
Cong Dang <congdang@asnet.com.vn>
2013-12-31 16:45:53 +0700

    create script for install hadoop and hive on ubuntu server

---
+++ install.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+# back to home folder (/home/user_name)
+cd ~/
+
+# download Hadoop 1.2.1 from official site
+wget -c http://mirrors.digipower.vn/apache/hadoop/common/hadoop-1.2.1/hadoop-1.2.1.tar.gz
+
+# untar the package.
+tar -xvf hadoop-1.2.1.tar.gz
+
+# download Hive 0.11.0 from official site
+wget -c http://mirrors.digipower.vn/apache/hive/stable/hive-0.11.0.tar.gz
+
+# untar the package
+tar -xvf hive-0.11.0.tar.gz
+
+# copy the environment file for set $HADOOP_HOME & $HIVE_HOME
+sudo cp -rf hadoop-hive-installing/config/environment /etc/environment
+
+# run source for set environment
+source /etc/environment
+
+# copy some config file for hadoop.
+cp -rf hadoop-hive-installing/config/hadoop/* $HADOOP_HOME/conf/
+cp -rf hadoop-hive-installing/config/hadoop/hdfs-site.xml $HADOOP_HOME/conf/hdfs-site.xml
+cp -rf hadoop-hive-installing/config/hadoop/mapred-site.xml $HADOOP_HOME/conf/mapred-site.xml
+
+# create hdfs folder
+mkdir hdfs
+sudo chmod -R 755 hdfs/
+
+# format hadoop hdfs
+$HADOOP_HOME/bin/hadoop namenode -format
+
+#start hadoop
+$HADOOP_HOME/bin/hadoop/start-all.sh
